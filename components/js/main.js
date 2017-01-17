6b236851afa83245abccaccc2d59f2195c9682ed
SPMSSICC <spmsicc@gmail.com>
2017-01-17 12:57:19 +0000

    update

--- components/js/main.js
+++ components/js/main.js
@@ -59,7 +59,7 @@
 				$(
 					'<div id="titleBar">' +
 						'<a href="#sidebar" class="toggle"></a>' +
-						'<span class="title">SGES Manual Utilizador</span>' +
+						'<span class="title">SICC - Documentação</span>' +
 					'</div>'
 				)
 					.appendTo($body);
