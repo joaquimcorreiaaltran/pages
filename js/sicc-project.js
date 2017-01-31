558ae371a8fc13b3fda6da6ecc49d6709b5b24e2
SPMSSICC <spmsicc@gmail.com>
2017-01-31 19:16:36 +0000

    update

--- js/sicc-project.js
+++ js/sicc-project.js
@@ -34,10 +34,6 @@ function convertMdToHtml(docName,elementId){
                                           $("#"+elementId).ready($("#"+elementId).html(converter.makeHtml(request.responseText)));//converte markdown para html e coloca o html no elemento #documento
                                           zommClickImagem();
                                        }/*if*/
-                                       else if(request.readyState == XMLHttpRequest.DONE && request.status === 404)
-                                       {
-                                          $("#"+elementId).html(msg_erro_1+"<p> </p><p>O ficheiro pedido não existe no servidor</p>");
-                                       }
                                        else {
                                           $("#"+elementId).html(msg_erro_1)
                                        }
