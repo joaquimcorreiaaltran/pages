8cffecec8f31928d9e7253f3a77dd781511a4a4b
SPMSSICC <spmsicc@gmail.com>
2017-01-31 19:12:14 +0000

    update

--- js/sicc-project.js
+++ js/sicc-project.js
@@ -26,7 +26,7 @@ hs.blockRightClick = true;
 //load and convert Markdown to Html and show it
 function convertMdToHtml(docName,elementId){
    var request = new XMLHttpRequest();
-   var msg_erro_1 = "<H2 style='text-align:center'>Não foi possível carregar o conteúdo :("+request.;
+   var msg_erro_1 = "<H2 style='text-align:center'>Não foi possível carregar o conteúdo :(";
    request.open('GET', '../markdown/'+docName+'.md',true);//Asynchronous request (true=asynchronous)
    request.onreadystatechange = function(){
                                        if(request.readyState == XMLHttpRequest.DONE && request.status === 200){
