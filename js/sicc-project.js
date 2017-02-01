1cd6a80e5729ef07e3be96f0c7d43445043ac13b
SPMSSICC <spmsicc@gmail.com>
2017-02-01 18:54:20 +0000

    update

--- js/sicc-project.js
+++ js/sicc-project.js
@@ -37,10 +37,10 @@ function convertMdToHtml(docName,elementId){
                                           $("#docButtons").ready(
                                              //$("#btnEditarDoc").attr("onclick", "window.open('https://github.com/SPMSSICC/pages/edit/master/content/markdown/"+docName+".md','_blank');");
                                              document.getElementById("btnEditarDoc").setAttribute("onclick", "window.open('https://github.com')")
+
+                                             alert($("#docButtons").ready());
                                           );
                                              //"window.open('https://github.com/SPMSSICC/pages/edit/master/content/markdown/"+docName+".md','_blank')"
-
-
                                        }/*if*/
                                        else if(request.readyState == XMLHttpRequest.DONE && request.status != 200){
                                           $("#"+elementId).html(msg_erro_1)
