9e4253de6a099dd0c94daca4eb97476addd96330
SPMSSICC <spmsicc@gmail.com>
2017-02-02 16:19:39 +0000

    lint hints

--- js/sicc-project.js
+++ js/sicc-project.js
@@ -3,38 +3,24 @@ Miscellaneous functions to use in SICC project (https://spmssicc.github.io/pages
 Feb-2017
 */
 
-//"use strict"; //Ajuda no debug
-
-//Configurations
-
-//highslide-with-gallery configs
-hs.fadeInOut = true;
-hs.align = 'center';
-hs.transitions = ['expand', 'crossfade'];
-hs.outlineType = 'rounded-white';//'rounded-white';'rounded-black'
-hs.fadeInOut = true;
-hs.numberPosition = 'caption';
-hs.dimmingOpacity = 0.5;
-hs.dimmingGeckoFix = true;
-hs.blockRightClick = true;
-
+"use strict"; //Ajuda no debug
 
 // functions
 
 //load and convert Markdown to Html and show it
-function convertMdToHtml(docName,elementId){
+function convertMdToHtml(docName, elementId) {
    var request = new XMLHttpRequest();
    var msg_erro_1 = "<H2 style='text-align:center'>Não foi possível carregar o conteúdo :(";
    request.open('GET', '../markdown/' + docName + '.md', true);//Asynchronous request (true=asynchronous)
    request.onreadystatechange = function () {
-                                       if (request.readyState === XMLHttpRequest.DONE && request.status === 200){
+        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
                                           var converter = new showdown.Converter(); //instancia
-                                          $("#"+elementId).ready($("#"+elementId).html(converter.makeHtml(request.responseText))); //converte markdown para html e coloca o html no elemento #documento
+                                          $("#" +elementId).ready($("#"+elementId).html(converter.makeHtml(request.responseText))); //converte markdown para html e coloca o html no elemento #documento
                                           zommClickImagem();
                                        }/*if*/
                                        else if(request.readyState == XMLHttpRequest.DONE && request.status != 200){
                                           $("#"+elementId).html(msg_erro_1);
-                                       }
+        }
                                  };/*function*/
    request.send();
 }
@@ -45,7 +31,7 @@ function zommClickImagem(){
     $('#documento img').each(function(){
                                  var alt = $(this).attr("alt");
                                  //if(alt != "figAlteracaoSenha" && alt != "figLogin" && alt !="figLoginRecuperacao")
-                                 $(this).wrap("<a class='imagem' href='"+$(this).attr( "src" ) + "' onclick='return hs.expand(this)'></a>");
+                                 $(this).wrap("<a class='imagem' href='"+$(this).attr("src") + "' onclick='return hs.expand(this)'></a>");
                               });
 }
 
@@ -65,20 +51,20 @@ function ocultaElemento(elementId) {
 var currFFZoom = 1;
 var currIEZoom = 100;
 
-function zoomInBrowser(){
-     if (isFirefox){
-         var FFstep = 0.02;
-         currFFZoom += step;
-         $('body').css('MozTransform','scale(' + currFFZoom + ')');
+function zoomInBrowser() {
+     if (isFirefox) {
+      var FFstep = 0.02;
+      currFFZoom += FFstep;
+         $('body').css('MozTransform', 'scale(' + currFFZoom + ')');
      } else {
          var IEstep = 2;
-         currIEZoom += step;
+         currIEZoom += IEstep;
          $('body').css('zoom', ' ' + currIEZoom + '%');
      }
  }
 
-function zoomOutBrowser(){
-     if (isFirefox){
+function zoomOutBrowser() {
+     if (isFirefox) {
          var FFstep = 0.02;
          currFFZoom -= step;
          $('body').css('MozTransform', 'scale(' + currFFZoom + ')');
