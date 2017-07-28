d768fa9faa78d51463b76b5998ae7a6f3b418e2c
SPMSSICC <SPMSSICC@users.noreply.github.com>
2017-07-28 10:49:22 +0100

    animação suave adicionada na navegação com índice

--- js/sicc-project.js
+++ js/sicc-project.js
@@ -211,3 +211,32 @@ window.onclick = function(event) {
     }
   }
 };
+
+
+
+
+/*********************************************************************
+override standard href-id navigation on page without change HTML markup
+for smooth scrolling
+*********************************************************************/
+
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
