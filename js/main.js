c04d834edb50d12c2dfc26e1cd969e4a5494d9fc
SPMSSICC <spmsicc@gmail.com>
2017-09-12 15:58:22 +0100

    correção da atualização anterior

--- js/main.js
+++ js/main.js
@@ -23,10 +23,10 @@ blockRightClick = true;
 /*falta adicionar suporte para as anchor de documentos e suporte para carregar iframes e  o load commit history*/
 function loadContent(){
 
-	//var fullURL = window.location.href; // Returns full URL
+	var fullURL = window.location.href; // Returns full URL
 
 	//var fullURL = "https://spmssicc.github.io/pages/index.html?doc=menus";
-	var fullURL = "https://spmssicc.github.io/pages/index.html?doc=processos&anchor=#213-mapas-lpca";//#41-tabelas-genéricas //#213-mapas-lpca
+	//var fullURL = "https://spmssicc.github.io/pages/index.html?doc=processos&anchor=#213-mapas-lpca";//#41-tabelas-genéricas //#213-mapas-lpca
 
 	var index = fullURL.indexOf("?");
 
