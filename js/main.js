83229bd8fe05c3556112d9d2fb21da1d7cea5381
SPMSSICC <spmsicc@gmail.com>
2017-09-13 18:44:32 +0100

    update last commit (decode uri)

--- js/main.js
+++ js/main.js
@@ -50,7 +50,8 @@ function loadContent(){
 							var anchor;
 
 							if (paramArr.length >= 2){
-									anchor = "#" + paramArr[1].substring(paramArr[1].indexOf("=") + 1, 99); // Returns anchor of the document
+									anchor = paramArr[1].substring(paramArr[1].indexOf("=") + 1, 99); // Returns anchor of the document
+									anchor = "#" + decodeURI(anchor);
 									console.log("\n\n\n\n\nENTROU AQUI! anchor:"+anchor+"\nparamArr[1].substring(paramArr[1].indexOf('=') + 1, 99): "+paramArr[1].substring(paramArr[1].indexOf('=') + 1, 99)+"\n\n\n\n");
 								}
 							console.log("[loadContent] doc: " + doc +
