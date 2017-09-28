211b08901bc2fdc2af11d841c65908dfaa5af242
SPMSSICC <spmsicc@gmail.com>
2017-09-28 14:34:37 +0100

    Funcionalidade de pesquisa

--- js/search.js
+++ js/search.js
@@ -1,54 +1,75 @@
-function Kateryna(){
+var arrDocNames = ['about','cer_migracao_sicc','chave_orcamental_por_ano','menus','processos'];
+var arrDocs = []; // array
 
-  var str = "cabimentos";
+//load all markdown documents synchronously
+function loadAllMdownDocs(){
+  $.each(arrDocNames, function(i, name){
+      $.get("./markdown/" + name +".md").done(
+          function(content) {
+            var doc = {name:"",content:""};
+            doc.name = name;
+            doc.content = content;
+            arrDocs.push(doc);
+          } //function
+        ); //done
+    } //function
+  ); //each
+}
+
+
+function kateryna(){
+
+  $("#resultsList").html("");
+
+  var str = $("#textToSearch")["0"].value;
   var regexp = new RegExp(str.toUpperCase(),"g");
-  var doc = {name:"",content:""}, // object with properties
-      arrDocs = []; // array
-  var htmlResults = "<div id='behindSearchMatches' class='active'><div id='searchMatches' class='search-matches active'>"+
-                      "<h3>Resultados da pesquisa:</h3>"+
-                      "<i onclick='loadContent()' title='Voltar à página inicial' class='fa fa-times fa-fw fa-2x' aria-hidden='true'></i>";
-
-    //load all markdown documents synchronously
-    var arrDocNames = ['about','cer_migracao_sicc','chave_orcamental_por_ano','menus','processos'];
-    jQuery.ajaxSetup({async:false}); // to execute the .get requests synchronously
-
-    $.each(arrDocNames, function(i, name){
-        $.get("./markdown/" + name +".md").done(
-            function(content) {
-              var doc = {name:"",content:""};
-              doc.name = name;
-              doc.content = content;
-              arrDocs.push(doc);
-            } //function
-          ); //done
-      } //function
-    ); //each
-
-    jQuery.ajaxSetup({async:true});
 
     var match, arrMatches = [];
+    var htmlTest = "<div id='resultsList'>";
 
-    $.each(arrDocs, function(i, d){
-          while ((match = regexp.exec(d.content.toUpperCase())) != null) {
+    if(str.length > 5){
 
-          var start = (match.index - 15), end = (match.index + 25);
+        $.each(arrDocs, function(i, d){
+              while ((match = regexp.exec(d.content.toUpperCase())) != null) {
+              var start = (match.index - 15), end = (match.index + 25);
+              htmlTest = htmlTest + '<li><a onclick="loadMdDoc(\'' + d.name + '\', [\'btnMenu\'],\'\', event)">'+
+                                          '\'...'+d.content.substring(start,end) +'...\' (ver documento: '+d.name+');'+'</a></li>';
 
-          htmlResults = htmlResults + '<li><a onclick="loadMdDoc(\'' + d.name + '\', [\'btnMenu\'],\'\', event)">'+
-                                      '\'...'+d.content.substring(start,end) +'...\' (ver documento: '+d.name+');'+'</a></li>';
+                  //arrMatches.push(d.name + "|" + match.index);
 
-              //arrMatches.push(d.name + "|" + match.index);
+              } //while
+            } //each
+        ); //each
+      }
+    htmlTest = htmlTest + "</div>";
 
-          } //while
-
-        } //each
-    ); //each
-
-    htmlResults = htmlResults + "</div></div>";
-
-    setTimeout(function () {
-      $("#content").before(htmlResults);
-    }, 100);
-
-
+  $("#btnSearch").after(htmlTest);
 
 }/*kateryna*/
+
+
+function startDictation() {
+
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = "pt-PT";
+    recognition.start();
+
+    recognition.onresult = function(e) {
+      document.getElementById('textToSearch').value = e.results[0][0].transcript;
+      recognition.stop();
+      console.log("onresult:", e.results[0][0].transcript);
+      document.getElementById('labnol').submit();
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+
+  }
+}
